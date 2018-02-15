8a4c9d784332bdda8bd9d1684776b6a727aca81e
Lisa Chan <xlisachan@gmail.com>
2018-02-15 23:14:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,14 @@ function whileLoop(number){
   }
   return 'done';
 }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+    array.pop();
+  }
+    while (array.length > 0 && maybeTrue());
+  return array;
+}
