f8fed4c765fc531ceaf377d29f772ec9067f531c
Lisa Chan <xlisachan@gmail.com>
2018-02-15 21:03:47 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+/*TODO: Build a function forLoop.
+It takes an array as an argument.
+Start counting from 0, and, using a for loop, add a string to the array 25 times.
+But not just any string.
+If your i value is 1, add the string "I am 1 strange loop.";
+if your i value is anything else, add the string "I am ${i} strange loops.".
+(Remember flow control with if and else? And how do we interpolate i?)
+Then return the array.*/
+
+function forLoop(array) {
+  for (let i = 0; i < 25, i++) {
+    if (i = 1){
+      console.log("I am 1 strange loop.");
+    } else {
+      console.log(`I am ${i} strange loops.`);
+    }
+    return array;
+  }
+}
