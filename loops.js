97efea45881ac743f4c5582edec229052f976c11
Lisa Chan <xlisachan@gmail.com>
2018-02-15 23:00:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,9 +10,10 @@ Then return the array.*/
 function forLoop(array){
   for (let array[i] = 0; array[i] < 25, array[i]++) {
     if (array[i] === 1){
-      console.log("I am 1 strange loop.");
+      array.push("I am 1 strange loop.");
     } else {
-      console.log(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops.`);
     }
   }
+  return array;
 }
